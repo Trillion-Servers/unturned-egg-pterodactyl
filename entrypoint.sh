2c7263b3c003f0817a95443c5aae436cacfa9abe
Griffindor301 <griffhoover@outlook.com>
2023-01-25 09:55:20 -0500

    Updates to uScript Auto Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ fi
 
 if [ "${USCRIPT_AUTOUPDATE}" == "1" ]; then
     wget https://trillionservers.com/unturned-egg/uScript.Unturned.zip
-	unzip -o -q uScript.Unturned.zip -d Modules && rm uScript.Unturned.zip
+	unzip uScript.Unturned.zip -d Modules && rm uScript.Unturned.zip
 fi
 
 mkdir -p Unturned_Headless_Data/Plugins/x86_64
